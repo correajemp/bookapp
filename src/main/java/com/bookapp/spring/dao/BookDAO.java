c24c5821514e2471b262b749d32d1ee944daf04e
correajemp <44298120+correajemp@users.noreply.github.com>
2019-01-10 10:09:23 +0800

    Initial commit

---
+++ src/main/java/com/bookapp/spring/dao/BookDAO.java
@@ -0,0 +1,25 @@
+package com.bookapp.spring.dao;
+
+import java.util.List;
+
+import com.bookapp.spring.model.Book;
+
+public interface BookDAO {
+
+	// Save the record
+	long save(Book book);
+	
+	// Get a single record
+	Book get(long id);
+	
+	// Get all the records
+	List<Book> list();
+	
+	// Update the record
+	void update(long id, Book book);
+	
+	// Delete a record
+	void delete(long id);
+	
+	
+}
